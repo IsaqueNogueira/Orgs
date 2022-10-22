d48a642d0bb0fa5eccc7b67f58bdcdd637b24b3b
Isaque Nogueira <109471754+IsaqueNogueira@users.noreply.github.com>
2022-10-22 12:06:21 -0300

    Primeiro commit

--- app/src/main/java/com/example/orgs/model/Produto.kt
+++ app/src/main/java/com/example/orgs/model/Produto.kt
@@ -1,2 +1,7 @@
-package com.example.orgs.model 
+package com.example.orgs.model
+
+import java.math.BigDecimal
+
+data class Produto(val nome : String, val descricao : String, val valor : BigDecimal)
+
 
--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,10 +2,64 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.example.orgs.R
+import com.example.orgs.dao.ProdutoDao
+import com.example.orgs.databinding.ActivityFormularioProdutoBinding
+import com.example.orgs.model.Produto
+import java.math.BigDecimal
 
-class FormularioProdutoActivity : AppCompatActivity() {
+class FormularioProdutoActivity :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    private val binding by lazy {
+        ActivityFormularioProdutoBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_produto)
+        setContentView(binding.root)
+        configuraBotaoSalvar()
+        binding.activityFormularioProdutoImagem.setOnClickListener{
+            AlertDialog.Builder(this)
+                .setView(R.layout.formulario_imagem)
+                .setPositiveButton("Confirmar") { _, _ ->
+
+                }
+                .setNegativeButton("Cancelar") {_,_ ->
+
+                }
+                .show()
+        }
+
+
     }
+
+    private fun configuraBotaoSalvar() {
+        val botaoSalvar = findViewById<Button>(R.id.activity_formulario_botao_salvar)
+        val dao = ProdutoDao()
+        botaoSalvar.setOnClickListener {
+            val produtoNovo = criaProduto()
+            dao.adiociona(produtoNovo)
+            finish()
+        }
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = findViewById<EditText>(R.id.activity_formulario_edit_nome)
+        val nome = campoNome.text.toString()
+        val campoDescricao = findViewById<EditText>(R.id.activity_formulario_edit_descricao)
+        val descricao = campoDescricao.text.toString()
+        val campoValor = findViewById<EditText>(R.id.activity_formulario_edit_valor)
+        val valorEmTexto = campoValor.text.toString()
+        val valor = if (valorEmTexto.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+
+        return Produto(nome = nome, descricao = descricao, valor = valor)
+    }
+
 }

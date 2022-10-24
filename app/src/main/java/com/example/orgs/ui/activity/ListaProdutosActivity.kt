67e0039fb95badd85da17310eca992f5137d4444
Isaque Nogueira <109471754+IsaqueNogueira@users.noreply.github.com>
2022-10-24 19:51:33 -0300

    Finalizando

--- app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -3,8 +3,11 @@ package com.example.orgs.ui.activity
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -28,6 +31,7 @@ class ListaProdutosActivity : AppCompatActivity() {
         setContentView(binding.root)
         configuraRecyclerview()
         configuraFab()
+
     }
 
 

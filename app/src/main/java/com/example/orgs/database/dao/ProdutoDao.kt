a1b8cdf655b9047619ccf19b35dc885118650457
Isaque Nogueira <109471754+IsaqueNogueira@users.noreply.github.com>
2022-10-27 13:19:02 -0300

    Finalizando room

--- app/src/main/java/com/example/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/com/example/orgs/database/dao/ProdutoDao.kt
@@ -16,4 +16,23 @@ interface ProdutoDao {
 
     @Query("SELECT * FROM Produto WHERE id = :id")
     fun buscaPorId(id: Long) : Produto?
+
+
+    @Query("SELECT * FROM Produto ORDER BY nome Desc")
+    fun nomeDesc() : List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY nome Asc")
+    fun nomeAsc() : List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY descricao Desc")
+    fun descricaoDesc() : List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY descricao Asc")
+    fun descricaoAsc() : List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY valor Desc")
+    fun valorDesc() : List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY valor Asc")
+    fun valorAsc() : List<Produto>
 }
--- app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -2,6 +2,8 @@ package com.example.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.orgs.R
@@ -16,6 +18,10 @@ class ListaProdutosActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityListaProdutosBinding.inflate(layoutInflater)
     }
+    private val produtoDao by lazy {
+        val db = AppDatabase.instancia(this)
+        db.produtoDao()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,48 @@ class ListaProdutosActivity : AppCompatActivity() {
         configuraRecyclerview()
         configuraFab()
 
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_lista_produtos, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.lista_nomedesc ->{
+              produtoDao.nomeDesc()
+                adapter.atualiza(produtoDao.nomeDesc())
+            }
+            R.id.lista_nomeasc ->{
+                produtoDao.nomeAsc()
+                adapter.atualiza(produtoDao.nomeAsc())
+            }
+            R.id.lista_descricaodesc ->{
+                produtoDao.descricaoDesc()
+                adapter.atualiza(produtoDao.descricaoDesc())
+            }
+            R.id.lista_descricaoasc ->{
+                produtoDao.descricaoAsc()
+                adapter.atualiza(produtoDao.descricaoAsc())
+            }
+            R.id.lista_valordesc ->{
+                produtoDao.valorDesc()
+                adapter.atualiza(produtoDao.valorDesc())
+            }
+            R.id.lista_valorasc ->{
+                produtoDao.valorAsc()
+                adapter.atualiza(produtoDao.valorAsc())
+            }
+            R.id.lista_semordem ->{
+                produtoDao.buscaTodos()
+                adapter.atualiza(produtoDao.buscaTodos())
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 
@@ -40,8 +88,6 @@ class ListaProdutosActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        val db = AppDatabase.instancia(this)
-        val produtoDao = db.produtoDao()
         adapter.atualiza(produtoDao.buscaTodos())
 
     }
